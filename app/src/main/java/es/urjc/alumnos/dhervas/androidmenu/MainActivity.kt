4c107ef50265b387b4338401093a5617bbc0c26a
dhervas <danielhervasrodao@gmail.com>
2022-02-23 10:32:13 +0100

    Initial commit

---
+++ app/src/main/java/es/urjc/alumnos/dhervas/androidmenu/MainActivity.kt
@@ -0,0 +1,22 @@
+package es.urjc.alumnos.dhervas.androidmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        setSupportActionBar(toolBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+}
